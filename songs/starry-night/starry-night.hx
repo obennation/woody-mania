f9ef0e5bc6db15d119e74e849823f471f44a4a5c
PipocaAventuras <benjaminsimonsencarvalho@gmail.com>
2026-06-18 15:11:31 -0300

    cutscene final!!

--- songs/starry-night/starry-night.hx
+++ songs/starry-night/starry-night.hx
@@ -1,4 +1,21 @@
+var endingPlayed:Bool = false;
+
 function onLoad()
 {
 	videoCutscene('weekwoody/starrynight', true);
+}
+
+function onEndSong()
+{
+    if (!endingPlayed)
+    {
+        endingPlayed = true;
+        endingCutscene = true;
+
+        videoCutscene('weekwoody/ending', false);
+
+        return Function_Stop;
+    }
+
+    return Function_Continue;
 }
