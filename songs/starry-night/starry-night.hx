098c7ff2d08214423ac1c341ee7a09b6b0c3a575
PipocaAventuras <benjaminsimonsencarvalho@gmail.com>
2026-06-25 18:53:31 -0300

    cutscene progamada

--- songs/starry-night/starry-night.hx
+++ songs/starry-night/starry-night.hx
@@ -1,5 +1,28 @@
 var endingPlayed:Bool = false;
 
+function onCreatePost()
+{
+	cutscene = new FunkinVideoSprite();
+	cutscene.onFormat(() -> {
+		cutscene.cameras = [camOther];
+		cutscene.setGraphicSize(FlxG.width);
+		cutscene.screenCenter();
+		add(cutscene);
+	});
+	cutscene.onEnd(() -> {
+		camGame.alpha = 1;
+		camHUD.alpha = 1;
+		camGame.flash(0xFFFFFFFF, 0.35);
+		cutscene.kill();
+	});
+	cutscene.load(Paths.video('weekwoody/mid-starrynight'), [FunkinVideoSprite.muted]);
+	cutscene.antialiasing = ClientPrefs.globalAntialiasing;
+	
+	cutscene.play();
+	cutscene.pause();
+	cutscene.tiedToGame = false;
+}
+
 function onLoad()
 {
 	videoCutscene('weekwoody/starrynight', true);
@@ -18,4 +41,19 @@ function onEndSong()
 	}
 
 	return Function_Continue;
+}
+
+function onEvent(name, v1, v2)
+{
+	switch (name)
+	{
+		case 'Mania':
+			switch (v1)
+			{
+				case 'cutscene':
+					cutscene.play();
+					cutscene.tiedToGame = true;
+					camGame.alpha = camHUD.alpha = 0;
+			}
+	}
 }
